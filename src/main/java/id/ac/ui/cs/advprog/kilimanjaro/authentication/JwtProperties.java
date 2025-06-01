0d8886735d8dee916f82e775111c877aa3bc8659
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-06-01 13:35:30 +0700

    feat[GREEN]: add settings for public and private key, implement RS256 signing algorithm.

--- src/main/java/id/ac/ui/cs/advprog/kilimanjaro/authentication/JwtProperties.java
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/authentication/JwtProperties.java
@@ -13,8 +13,13 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class JwtProperties {
-    private String secret;
+    // Remove secret key for HS256
+    // private String secret;
+
+    // Add private and public keys for RS256
+    private String privateKey;  // Could be base64 or PEM encoded
+    private String publicKey;   // Could be base64 or PEM encoded
+
     private long accessExpiration;
     private long refreshExpiration;
 }
-
